3eb877e07835f0bc41530d965890c9bdc887b5ab
FaithD186 <faith.dong@mail.utoronto.ca>
2024-01-24 00:44:24 -0500

    Edited button css and media screen margins, edited recipes json file.

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -101,9 +101,11 @@ function Homepage() {
   return (
     <div className="section">
       <h2 class="text-4xl font-extrabold dark:text-white">Recipe Buddy</h2>
-      <p class="my-4 text-lg text-gray-500">Keep track of your trusted go-to recipes, recipes to try, and recipes that hold a special place in your heart.</p>
+      <p class="my-4 text-lg text-gray-500">Keep track of your tried-and-true recipes, recipes to try, and recipes that hold a special place in your heart.</p>
       {showAddRecipe && (
-        <button class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={addRecipe}>+ Add Recipe</button>
+        <button class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-base px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={addRecipe}>
+          + Add Recipe
+        </button>
       )}
       {showForm && (
         <div class="w-full max-w-6xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
@@ -172,13 +174,13 @@ function Homepage() {
                 <p class="text-lg font-semibold text-blue-600/75 dark:text-blue-500/75">Steps</p>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{recipe.steps}</p>
                 <br></br>
-                <button  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                <button  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     onClick={() => setEditForm(recipe.id === showEditForm
                     ? null 
                     : recipe.id)}>
                     Edit Recipe
                 </button>
-                <button onClick={()=> deleteRecipe(recipe.id)} class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                <button onClick={()=> deleteRecipe(recipe.id)} class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                   Remove
                 </button>
             </div>
